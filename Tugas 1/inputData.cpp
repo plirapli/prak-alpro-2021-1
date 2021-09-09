37cc61d709ada0c1cf56bb2f5e9ee02beba8aafd
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-09-09 12:11:39 +0700

    init commit

---
+++ Tugas 1/inputData.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+     string nama, nim, plug;
+     int jmlStrip;
+
+     // Input Data
+     cout << "Masukan"
+          << "\n"
+          << "----------"
+          << "\n";
+
+     cout << "Nama" << setw(6) << ": ";
+     getline(cin, nama);
+
+     cout << "NIM" << setw(7) << ": ";
+     getline(cin, nim);
+
+     cout << "Plug" << setw(6) << ": ";
+     getline(cin, plug);
+
+     // Output data
+     cout << "\n"
+          << "Inilah data Anda :"
+          << "\n";
+
+     cout << "Nama" << setw(6) << ": " << nama << "\n";
+     cout << "NIM" << setw(7) << ": " << nim << "\n";
+     cout << "Plug" << setw(6) << ": " << plug << "\n";
+
+     // Menghitung total karakter untuk menentukan strip (=)
+     jmlStrip = nama.length() + nim.length() + plug.length() + 6 + 11;
+
+     // Tabel
+     // ======= Header =======
+     cout << "\n"
+          << "Dalam bentuk tabel :" << endl;
+
+     cout << setfill('=') << setw(jmlStrip - 2) << "="
+          << "\n";
+
+     cout << setfill(' ')
+          << "NIM" << setw(nim.length())
+          << "|" << setw(6) << "Nama" << setw(nama.length())
+          << "|" << setw(6) << "Plug" << setw(plug.length()) << "|"
+          << "\n";
+
+     cout << setfill('=') << setw(jmlStrip - 2) << "="
+          << "\n";
+
+     // ====== Row =======
+     cout << setfill(' ')
+          << nim << setw(3)
+          << "|" << setw(nama.length() + 2) << nama << setw(4) << "|"
+          << setw(plug.length() + 2) << plug << setw(4) << "|"
+          << "\n";
+
+     cout << setfill('=') << setw(jmlStrip - 2) << "="
+          << "\n";
+
+     return 0;
+}
---
+++ Tugas 1/suhuC.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  float r, f, c, k;
+
+  // Konversi Suhu dari Celsius
+  cout << "Konversi Suhu C++"
+       << "\n";
+  cout << "Masukkan Suhu (Celsius) : ";
+  cin >> c;
+
+  r = (c * 4 / 5);
+  f = (c * 9 / 5) + 32;
+  k = c + 273;
+
+  cout << "Reamur" << setw(7) << ": " << r << " derajat R"
+       << "\n";
+  cout << "Fahrenheit" << setw(3) << ": " << f << " derajat F"
+       << "\n";
+  cout << "Kelvin" << setw(7) << ": " << k << " derajat K"
+       << "\n";
+
+  return 0;
+}
