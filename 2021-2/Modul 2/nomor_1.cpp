207d113085538673b47e9e8269fd454f6f175214
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-02-24 00:00:29 +0700

    refactor

--- 2021-2/Modul 2/nomor_1.cpp
+++ 2021-2/Modul 2/nomor_1.cpp
@@ -9,6 +9,7 @@ struct Pegawai
 } pegawai[10];
 
 void showPegawai(Pegawai pegawai[], int pegawaiAktif);
+void addPegawai(Pegawai pegawai[], int &pegawaiAktif);
 
 int main()
 {
@@ -18,8 +19,8 @@ int main()
 	do
 	{
 		cout << "Menu: \n"
-				 << "[1] Tambah Pegawai \n"
-				 << "[2] Lihat Pegawai \n"
+				 << "[1] Lihat Pegawai \n"
+				 << "[2] Tambah Pegawai \n"
 				 << "[Others] Exit \n"
 				 << "Input > ";
 		cin >> inMenu;
@@ -28,34 +29,11 @@ int main()
 		switch (inMenu)
 		{
 		case '1':
-		{
-			cout << "Nama: ";
-			cin.ignore();
-			getline(cin, pegawai[pegawaiAktif].nama);
-
-			cout << "NIP: ";
-			cin >> pegawai[pegawaiAktif].nip;
-
-			cout << "Hari Kerja: ";
-			cin >> pegawai[pegawaiAktif].hariKerja;
-
-			cout << "Gaji: ";
-			cin >> pegawai[pegawaiAktif].gaji;
-
-			pegawai[pegawaiAktif].totalGaji =
-					pegawai[pegawaiAktif].gaji *
-					pegawai[pegawaiAktif].hariKerja;
-
-			cout
-					<< "\n"
-					<< "Data berhasil ditambahkan. \n\n";
-
-			pegawaiAktif++;
+			showPegawai(pegawai, pegawaiAktif);
 			break;
-		}
 
 		case '2':
-			showPegawai(pegawai, pegawaiAktif);
+			addPegawai(pegawai, pegawaiAktif);
 			break;
 
 		default:
@@ -71,13 +49,44 @@ int main()
 
 void showPegawai(Pegawai pegawai[], int pegawaiAktif)
 {
-	cout << "Daftar Pegawai: \n";
-	for (int i = 0; i < pegawaiAktif; i++)
+	if (pegawaiAktif > 0)
 	{
-		cout << "NIP: " << pegawai[i].nip << "\n"
-				 << "Nama: " << pegawai[i].nama << "\n"
-				 << "Hari Kerja: " << pegawai[i].hariKerja << "\n"
-				 << "Gaji harian: " << pegawai[i].gaji << "\n"
-				 << "Total Gaji: " << pegawai[i].totalGaji << "\n\n";
+		cout << "Daftar Pegawai: \n";
+		for (int i = 0; i < pegawaiAktif; i++)
+		{
+			cout << "NIP: " << pegawai[i].nip << "\n"
+					 << "Nama: " << pegawai[i].nama << "\n"
+					 << "Hari Kerja: " << pegawai[i].hariKerja << "\n"
+					 << "Gaji harian: " << pegawai[i].gaji << "\n"
+					 << "Total Gaji: " << pegawai[i].totalGaji << "\n\n";
+		}
 	}
+	else
+		cout << "Pegawai belum ada. \n\n";
+}
+
+void addPegawai(Pegawai pegawai[], int &pegawaiAktif)
+{
+	cout << "Nama: ";
+	cin.ignore();
+	getline(cin, pegawai[pegawaiAktif].nama);
+
+	cout << "NIP: ";
+	cin >> pegawai[pegawaiAktif].nip;
+
+	cout << "Hari Kerja: ";
+	cin >> pegawai[pegawaiAktif].hariKerja;
+
+	cout << "Gaji: ";
+	cin >> pegawai[pegawaiAktif].gaji;
+
+	pegawai[pegawaiAktif].totalGaji =
+			pegawai[pegawaiAktif].gaji *
+			pegawai[pegawaiAktif].hariKerja;
+
+	cout
+			<< "\n"
+			<< "Data berhasil ditambahkan. \n\n";
+
+	pegawaiAktif++;
 }
